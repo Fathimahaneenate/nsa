a8e41a69842f3d690cc7be4d516567254a4ccebc
Fathimahaneenate <117737547+Fathimahaneenate@users.noreply.github.com>
2023-06-07 14:41:41 +0530

    Add files via upload

---
+++ arith.sh
@@ -0,0 +1,12 @@
+echo "enter the first number"
+read a
+echo "enter the second number"
+read b
+sum=$((a + b))
+dif=$((a - b))
+mult=$((a * b))
+div=$((a / b))
+echo "sum:" $sum
+echo "difference:" $dif
+echo "product:" $mult
+echo "quotient:" $div
