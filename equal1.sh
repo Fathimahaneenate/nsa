6f691022bcec636bbfdecf1fa063b6957d9ae143
Fathimahaneenate <117737547+Fathimahaneenate@users.noreply.github.com>
2023-07-13 10:45:25 +0530

    Add files via upload

---
+++ equal1.sh
@@ -0,0 +1,15 @@
+
+
+echo "Enter first number:"
+read a
+echo "Enter second number:"
+read b
+if [ $a -gt $b ]
+then
+echo $a" is greater"
+elif [ $b -gt $a ]
+then
+echo $b" is greater"
+else
+echo "numbers are equal"
+fi
